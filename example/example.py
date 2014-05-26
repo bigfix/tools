4f8305a2308e87f19a7997246bdeeca8db60f9fb
Brian Green <briangreenery@gmail.com>
2014-05-26 15:38:07 -0700

    Make it possible to add extra usage

--- example/example.py
+++ example/example.py
@@ -5,11 +5,8 @@ from argparse import ArgumentParser
 from getpass import getpass
 
 class BigFixArgParser(ArgumentParser):
-  name = "hodor.py [options]"
-
-  def __init__(self):
-    description = "A tool for creating a smarter planet"
-    usage = """Options:
+  name = "Usage: hodor.py [options]"
+  base_usage = """Options:
   -h, --help                  Print this help message and exit
   -s, --server SERVER[:PORT]  REST API server and port
   -u, --user USER[:PASSWORD]  REST API user and password
@@ -17,17 +14,25 @@ class BigFixArgParser(ArgumentParser):
   -c, --cacert FILE           CA certificate used to verify the server's HTTPS
                               certificate"""
 
+  def __init__(self):
+    description = "A tool for creating a smarter planet"
+
     super(BigFixArgParser, self).__init__(add_help=False,
-      usage=usage, description=description)
+      usage=self.base_usage, description=description)
 
     self.add_argument('-k', '--insecure', action='store_true')
     self.add_argument('-c', '--cacert')
     self.add_argument('-u', '--user', required=True)
     self.add_argument('-s', '--server', required=True)
+    self.tool_usage = None
 
   def parse_args(self):
+    combined_usage = self.base_usage
+    if self.tool_usage is not None:
+      combined_usage += "\n" + self.tool_usage
+
     self.usage = "{0}\n\n{1}\n\n{2}".format(self.name,
-      self.description, self.usage)
+      self.description, combined_usage)
 
     if '-h' in sys.argv or '--help' in sys.argv:
       print(self.usage)
@@ -42,4 +47,5 @@ class BigFixArgParser(ArgumentParser):
     return args
 
 parser = BigFixArgParser()
+parser.tool_usage = """  -a, --apple                 Apples are cool"""
 print(parser.parse_args())
