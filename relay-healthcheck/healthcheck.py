9e21fcc9b83e9471a503d34c558a945c647336f7
butterfield <jeffreybutterfield@gmail.com>
2014-05-28 17:04:43 -0700

    Incorporated BigFixArgParser, usedsimpler authorizaiton, cleaned up

--- relay-healthcheck/healthcheck.py
+++ relay-healthcheck/healthcheck.py
@@ -1,83 +1,77 @@
 from bs4 import BeautifulSoup
 import base64
 import urllib
-import sys
+import sys, os
 from urlparse import urlparse
 import requests
 import json
 import argparse
 import getpass
 
+# Allow relative import of BigFixArgParser
+sys.path.append(os.path.abspath(os.path.join("../example")))
+from example import BigFixArgParser
+
 def main():
+    args = BigFixArgParser().parse_args()
+    auth = requests.auth.HTTPBasicAuth(args.user, args.password);
+    health_checks = load_health_checks()
+    evaluate_health_checks(args.server, auth, health_checks)
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--username")
-	parser.add_argument("--server")
-	if len(sys.argv) == 1:
-	    parser.print_help()
-	    sys.exit(1)
-	args = parser.parse_args()
-	serv = args.server
-	username = args.username
-	password = getpass.getpass("password:")
-	authHeader = createSession(username, password)
-	healthChecks = loadHealthChecks()
-	evaluateHealthChecks(serv, authHeader, healthChecks)
+def load_health_checks():
+    json_checks = open("healthchecks.json")
+    check_data = json.load(json_checks)
+    json_checks.close()
+    return check_data
 
-def loadHealthChecks():
-	jsonchecks = open("healthchecks.json")
-	checkdata = json.load(jsonchecks)
-	jsonchecks.close()
-	return checkdata
+def make_request(url, relevance, auth):
+    try:
+        r = requests.get(url,
+                auth=auth,
+                verify=False,
+                params = relevance)
+        if r.status_code != 200:
+            sys.stderr.write("Error: Bad response from server. Code: " +
+                             str(r.status_code))
+            sys.exit(1)
+        return r
+    except requests.exceptions.ConnectionError:
+        sys.stderr.write("Could not connect to REST api.")
+        sys.exit(1)
 
-def createSession(username, password):
-	s = requests.Session()
-	base64string = base64.encodestring('%s:%s' % (username, password))[:-1]
-	return  "Basic %s" % base64string
+def fetch_status_result(serv, relevance, authHeader):
+    res = make_request("https://" + serv + "/api/query",
+                       {"relevance": relevance} ,authHeader).text
+    resBS = BeautifulSoup(res)
+    if resBS.query.result.answer is None:
+        resstatus = format_status("False")	
+    else:
+        resstatus = format_status(resBS.query.result.answer.text)
+    return resstatus
 
-def request(url, relDict, authHeader):
-	try:
-	    r = requests.get(url, 
-	            headers={'Authorization': authHeader},
-	            verify=False,
-	            params = relDict)
-	    if r.status_code != 200:
-	    	sys.stderr.write("Error: Received bad response from server. Code: " + str(r.status_code))
-	    	sys.exit(1)
-	    return r
-	except requests.exceptions.ConnectionError:
-		sys.stderr.write("Could not connect to REST api.")
-		sys.exit(1)
+def format_status(ans):
+    if ans == "True":
+        return "Failed"
+    elif ans == "False":
+        return "Passed"
+    else:
+        return ans
 
-def fetchStatusResult(serv, rel, authHeader):
-	res = request('https://' + serv + ':52311/api/query', {'relevance': rel} ,authHeader).text
-	resBS = BeautifulSoup(res)
-	passFailAnswer = None
-	passFailAnswer = resBS.query.result.answer.text
-	resstatus = formatStatus(passFailAnswer)
-	return resstatus
-
-def formatStatus(ans):
-	if ans == "True":
-		return "Failed"
-	elif ans == "False":
-		return "Passed"
-	else:
-		return ans
-
-def evaluateHealthChecks(serv, authHeader, healthChecks):
-	passCount = 0
-	totalCount = 0
-	for section in healthChecks["HealthCheckSections"]:
-		print "++" + section["SectionName"] + "++"
-		for check in section["checklist"]:
-			status = fetchStatusResult(serv, check["checkrel"], authHeader)
-			print "\t" + status.upper() + "\t\t" + check["name"] + " - " + check["severity"] 
-			if status == "Passed":
-				passCount += 1
-			totalCount += 1
-	print "\nHealth Check Complete"
-	print "Checks Passed:" + str(passCount) + "/" + str(totalCount)
+def evaluate_health_checks(serv, auth, healthChecks):
+    fetch_status_result(serv, "0", auth)
+    passCount = 0
+    totalCount = 0
+    for section in healthChecks["HealthCheckSections"]:
+        print "++" + section["SectionName"] + "++"
+        for check in section["checklist"]:
+            status = fetch_status_result(serv, check["checkrel"], auth)
+            print ("\t" + status.upper() + "\t\t" + check["name"] +
+                   " - " + check["severity"]) 
+            if status == "Passed":
+                passCount += 1
+            totalCount += 1
+    print "\nHealth Check Complete"
+    print "Checks Passed:" + str(passCount) + "/" + str(totalCount)
 
 if __name__ == "__main__":
     main()
