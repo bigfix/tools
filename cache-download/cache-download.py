49dcfa1d48ca2dc7b51bff6270cccf9366edf714
Derek Leung <leungd@us.ibm.com>
2014-05-29 15:30:32 -0700

    Add options to cache-download.py

--- cache-download/cache-download.py
+++ cache-download/cache-download.py
@@ -15,7 +15,7 @@ FETCH_ALL_GATHER_QUERY = '(name of it, urls of it) of bes sites whose (external 
 FIXLET_REGEXP = '(http://.*\.fxf)'
 PREFETCH_REGEXP = 'prefetch\s+(\S+)\s+(sha1:\S+)\s+(size:\d+)\s+(\S+)\s+(sha256:\S+)?'
 CACHE_LOCATION = '/var/opt/BESServer/wwwrootbes/bfmirror/downloads/sha1/'
-# CACHE_LOCATION = ''
+CACHE_LOCATION = ''
 # TODO location of file cache is /var/opt/BESServer/besserver.config
 
 args = {}
@@ -31,29 +31,37 @@ def run():
     parser.description = 'A script to pre-fetch downloads to a root server'
     parser.base_usage += """
   -v, --verbose               Print more output
-  -n, --dry-run               Only print names of files to cache
-  -x, --sites [SITES]         Specify a list of sites to prefetch downloads from
+  -d, --dry                   Only print names of files to cache
+  -x, --sites [SITES]         Specify a list of sites to prefetch from
   -m, --maxsize [SIZE]        Avoids caching files with sizes larger than SIZE in bytes
-  -d, --name [REGEX]          Specify a regular expression of filenames to prefetch for
-  -r, --url [REGEX]           Specify a regular expression of URLs to look at"""
-    parser.add_argument('-x', '--sites', default = None, nargs = '*', help='a list of sites to pre-fetch downloads from (default is any site)')
-    parser.add_argument('-m', '--maxsize', default = GIGABYTE, help='avoid caching files larger than some size, in bytes (default is 1GB)')
-    parser.add_argument('-d', '--name', help='only download files whose names contain some regular expression')
-    parser.add_argument('-r', '--url', help='only download files whose urls contain some regular expression')
+  -t, --type [EXTENSION]      Specify a filetype extension to prefetch for
+  -r, --url [URL]             Specify a base URL to prefetch from
+  -n, --names [NAMES]         Specify a list of filenames to prefetch"""
+    parser.add_argument('-v', '--verbose', default=False, action='store_true', help='enable extra output')
+    parser.add_argument('-d', '--dry', default=False, action='store_true', help='only print list of files to download')
+    parser.add_argument('-x', '--sites', default=None, nargs='*', help='a list of sites to pre-fetch downloads from (default is any site)')
+    parser.add_argument('-m', '--maxsize', default=GIGABYTE, type=int, help='avoid caching files larger than some size, in bytes (default is 1GB)')
+    parser.add_argument('-t', '--type', help='only download files with a certain extension')
+    parser.add_argument('-r', '--url', help='only download files with some base URL')
+    parser.add_argument('-n', '--names', nargs='*', help='only download files with some name')
     args = parser.parse_args()
 
-    user_str = args.user.split(':')
     username, password = args.user.split(':')
     secure = not args.insecure
+    if not args.type is None:
+        args.type = args.type.lower()
+        if args.type[0] == '.':
+            args.type = args.type[1:]
 
     session = requests.Session()
-    print args
-
-    c = 0
 
     already_prefetched = set()
     gathers = fetch_gather_urls()
     for site in gathers:
+        if not (args.sites is None or site in args.sites):
+            continue
+        elif args.verbose:
+            print 'prefetching from site {} ({})'.format(site, gathers[site])
         fixlet_urls = fetch_fixlet_urls(gathers[site])
         for url in fixlet_urls:
             prefetch_actions = fetch_fixlet_prefetch_actions(url.group())
@@ -65,6 +73,13 @@ def run():
 
                 if size > args.maxsize:
                     continue
+                if not (args.type is None or filename.lower().endswith('.' + args.type)):
+                    continue
+                if not (args.url is None or download.startswith(args.url)):
+                    continue
+                if not (args.names is None or filename in args.names):
+
+                    continue
 
                 if sha1 in already_prefetched:
                     continue
@@ -72,12 +87,14 @@ def run():
                     already_prefetched.add(sha1)
 
                 target = CACHE_LOCATION + sha1
-                print 'downloading {}...'.format(download)
-                # urllib.urlretrieve(download, target)
-                print 'saved file {} [{}] to {}'.format(filename, size, target)
-                c = c + 1
-                if c == 5:
-                    return
+                if args.dry:
+                    print 'will download {} [{}] from {} to {}'.format(filename, size, download, target)
+                else:
+                    if args.verbose:
+                        print 'downloading {}...'.format(download)
+                    urllib.urlretrieve(download, target)
+                    if args.verbose:
+                        print 'saved file {} [{}] to {}'.format(filename, size, target)
 
 def fetch_fixlet_prefetch_actions(url):
     response = requests.get(url, auth=(username, password), verify=secure)
