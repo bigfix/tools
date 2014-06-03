eb5df36a920b8c13072ed5ce800f6a30a1953bdc
young-geng <young.gengxy@gmail.com>
2014-06-03 15:10:07 -0700

    creat analysis finished

--- create-analysis/create_analysis.py
+++ create-analysis/create_analysis.py
@@ -1,6 +1,7 @@
 from sys import stdin, stdout
 import sys
 import datetime
+import time
 import re
 from BeautifulSoup import BeautifulSoup as bs
 from argparse import ArgumentParser
@@ -9,7 +10,7 @@ import requests
 
 
 
-
+""" The command line argument parser """
 class BigFixArgParser(ArgumentParser):
   name = "Usage: create_analysis.py [options]"
   base_usage = """Options:
@@ -20,10 +21,11 @@ class BigFixArgParser(ArgumentParser):
   -u, --user                  REST API user
   -p, --password              REST API password
   -t, --site_type             Site type: master/custom/operator/external
-  -n, --site_name         Site-name
+  -n, --site_name             Site-name
+  -k, --insecure              Ignore SSL verification
 
   Note:
-    This application takes the relevance from stdin!"""
+    This application takes the relevance from stdin, each query statement should be in one line"""
 
   def __init__(self):
     description = "A tool for creating a smarter planet"
@@ -63,25 +65,44 @@ class BigFixArgParser(ArgumentParser):
 
 
 
+""" Compose analysis with template
+    param:
+      name: the name of the analysis, in string format
+      description: the description of the analysis, in string format
+      relevance: a list of relevance queries, each of which is a string
+    return:
+      the composed XML analysis as a string """
 def compose_analysis(name, description, relevance):
   time = str(datetime.datetime.now())[:10]
-  return """<?xml version="1.0" encoding="UTF-8"?>
-  <BES xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="BES.xsd">
-    <Analysis>
-      <Title>{0}</Title>
-      <Description>{1}</Description>
-      <Relevance>{2}</Relevance>
-      <Source>Internal</Source>
-      <SourceReleaseDate>{3}</SourceReleaseDate>
-      <Domain>BESC</Domain>
-      <Property Name="New Property" ID="1">exists BES computer</Property>
-    </Analysis>
-  </BES>
-  """.format(name, description, relevance, time)
+
+  analysis = """<?xml version="1.0" encoding="UTF-8"?>
+<BES xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="BES.xsd">
+<Analysis>
+<Title>{0}</Title>
+<Description>{1}</Description>
+<Relevance>true</Relevance>
+<Source>Internal</Source>
+<SourceReleaseDate>{2}</SourceReleaseDate>
+<Domain>BESC</Domain>\n""".format(name, description, time)
+  count = 1
+  for line in relevance:
+      analysis += '<Property Name="Analysis {0}" ID="{0}">{1}</Property>\n'.format(str(count), line)
+      count += 1
+  analysis += """</Analysis>\n</BES>"""
+  return analysis
 
 
 
-
+""" Create the analysis on server
+  param:
+    server: server address and port as a string, should be "server:port"
+    site_type: a string indicating the type of site, should be one of [master, external, operator, custom]
+    user: username as string
+    password: password as string
+    analysis: composed XML document of analysis
+    insecure: boolean value indicating whether to use insecure connection
+  return:
+    the text response from server """
 def create_analysis(server, site_type, site_name, user, password, analysis, insecure):
   if site_type == "master":
     site = "https://{0}/api/analyses/{1}".format(server, site_type)
@@ -91,6 +112,11 @@ def create_analysis(server, site_type, site_name, user, password, analysis, inse
   return r.text
 
 
+""" Find the ID string in a server reply, if no ID is found, print notice and exit
+  param:
+    reply: the server reply text, in string format
+  return:
+    the ID string """
 def find_id(reply):
   found = re.findall(r'<ID>[0-9]*?</ID>', reply)
   if len(found) == 0:
@@ -98,30 +124,56 @@ def find_id(reply):
     exit(1)
   return found[0][4: len(found[0]) - 5]
 
-def print_result(ID, server, user, password, insecure):
-  property_id = requests.post('https://{0}/api/query'.format(server), auth=(user, password), verify=not insecure,
-                              data={'relevance': '(ids of it as string) of bes properties whose (item 1 of id of it = {0})'.format(ID)}
-                             ).text
-  property_id = re.findall(r'string">([0-9, ]*?)<', property_id)
-  for line in property_id:
+
+""" Print the analysis evaluation result
+  param:
+    ID: the ID number of analysis, in string format
+    server: server address and port as a string, should be "server:port"
+    user: username as string
+    password: password as string
+    insecure: boolean value indicating whether to use insecure connection
+    query_length: Integer value of the number of statements in the analysis input
+  return:
+    None """
+def print_result(ID, server, user, password, insecure, query_length):
+  computers = requests.post('https://{0}/api/query'.format(server), auth=(user, password), verify=not insecure,
+                        data={'relevance': '(it as string) of properties whose (it as string contains "bes computers")'}
+                        ).text
+  computer_num = len(re.findall(r'string">([^>]*?)<', computers))
+  while True:
     result = requests.post('https://{0}/api/query'.format(server), auth=(user, password), verify=not insecure,
-                        data={'relevance': '(name of computer of it, value of it) of results of bes properties whose (id of it = ({0}))'.format(line)}
+                        data={'relevance': '(name of computer of it, value of it) of results of bes properties whose (item 1 of id of it = ({0}))'.format(ID)}
                         ).text
     result = re.findall(r'string">([^>]*?)<', result)
-    print "Computer name:  " + result[0]
-    print "     Result:  " + result[1] + "\n"
+    if len(result) / 2 == computer_num * query_length:
+      print "Report collected!\n\n"
+      count = 0
+      while count < len(result):
+        print "Computer name:  " + result[count]
+        print "     Result:  " + result[count + 1] + "\n"
+        count += 2
+      break
+    print "Waiting for report, got {0} / {1}".format(len(result) / 2, computer_num * query_length)
+    time.sleep(10)
 
 
 
 
 def main():
+  """ Parse the args """
   args = BigFixArgParser().parse_args()
+  """ Read the relevance from stdin and filter out empty lines """
   relevance = stdin.read().splitlines()
+  relevance = filter(lambda s: s.strip() != '', relevance)
+  """ Compose the analysis XML """
   analysis = compose_analysis(args.analysis_name, args.description, relevance)
+  """ Create the analysis on server, get reply """
   reply = create_analysis(args.server, args.site_type, args.site_name,
                           args.user, args.password, analysis, args.insecure)
+  """ Search for ID in the reply """
   ID = find_id(reply)
-  print_result(46, args.server, args.user, args.password, args.insecure)
+  """ Get analysis evaluation result """
+  print_result(ID, args.server, args.user, args.password, args.insecure, len(relevance))
 
 
 
